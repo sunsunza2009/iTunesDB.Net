acb167e462ad6cd5b69336517d583dce041c4fc0
Klaus Moster <moster@69grad.de>
2019-07-05 15:43:27 +0200

    Added an IntReader

---
+++ iTunesDB.Net/Readers/DataObjects/IntReader.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using iTunesDB.Net.Database;
+using DO = iTunesDB.Net.Enumerations.DataObjects;
+
+namespace iTunesDB.Net.Readers.DataObjects
+{
+    internal class IntReader : DataObjectReader
+    {
+        private static DO[] dataObjectTypes = new DO[] { DO.SizingAndOrder };
+        public override DO[] DataObjectTypes { get { return dataObjectTypes; } }
+
+        public override void ParseDataObject(BinaryReader Reader)
+        {
+            var dotype = ((DataObject)DbObject).Type;
+            var obj = ParentDbObject;
+
+            var unk1 = ReadInt32(Reader);
+            var unk2 = ReadInt32(Reader);
+            var position = ReadInt32(Reader);
+            var length = ReadInt32(Reader);
+            var unk3 = ReadInt32(Reader);
+            var unk4 = ReadInt32(Reader);
+
+            SetDataObjectString(dotype, position);
+        }
+    }
+}
