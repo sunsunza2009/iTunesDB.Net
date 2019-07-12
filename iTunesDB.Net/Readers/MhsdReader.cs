bc195d55e48d00d80977cc14b46fef6775dcfd40
Klaus Moster <moster@69grad.de>
2019-07-12 17:55:31 +0200

    Added more details to the mhsd reader.

--- iTunesDB.Net/Readers/MhsdReader.cs
+++ iTunesDB.Net/Readers/MhsdReader.cs
@@ -16,7 +16,16 @@ namespace iTunesDB.Net.Readers
             var listContainer = (ListContainer)DbObject;
             var db = (iTunesDb)ParentDbObject;
 
+            // 0x0D (4 bytes) - list type
+            //     1 = Track list - contains an MHLT
+            //     2 = Playlist List - contains an MHLP
+            //     3 = Podcast List - optional, probably. Contains an MHLP also.
+            //         This MHLP is basically the same as the full playlist section,
+            //         except it contains the podcasts in a slightly different way.
+            //     4 = Album List, first seen with iTunes 7.1.
+            //     5 = New Playlist List with Smart Playlists, first seen with iTunes 7.3.
             listContainer.ListType = ReadEnum<ListTypes>(Reader);
+
             db.ListContainers.Add(listContainer);
             return true;
         }
