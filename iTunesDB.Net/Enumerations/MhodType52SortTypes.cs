dcfc87fc5b06d01dd3c33a5c38271b30ddbda92a
Klaus Moster <moster@69grad.de>
2019-08-15 15:20:44 +0200

    Adds mhod types enum

---
+++ iTunesDB.Net/Enumerations/MhodType52SortTypes.cs
@@ -0,0 +1,14 @@
+﻿namespace iTunesDB.Net.Enumerations
+{
+    public enum MhodType52SortTypes
+    {
+        Title = 0x03,
+        Album = 0x04,
+        Artist = 0x05,
+        Genre = 0x07,
+        Composer = 0x12,
+        TvShow = 0x1d,
+        TvSeason = 0x1e,
+        TvEpisode = 0x1f
+    }
+}
